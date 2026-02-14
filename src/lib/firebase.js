77ad8a0dfb3e3eb470f765952817e258b95be5ee
Ramesh U <ramesh.u@transnationalai.com>
2026-02-14 16:40:36 +0530

    firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,10 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey:
+    import.meta.env.VITE_FIREBASE_API_KEY ||
+    "AIzaSyDvnP9aHuxPpcofdVVnxyonk_OajgH5mR4",
+  authDomain:
+    import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ||
+    "portfolio-utti.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "portfolio-utti",
+  appId:
+    import.meta.env.VITE_FIREBASE_APP_ID ||
+    "1:143702050213:web:129227786b58945fba544c",
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
